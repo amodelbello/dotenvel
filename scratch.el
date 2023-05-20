acc9712c88b968187f393e588ab92c38fb42f610
Amo DelBello <amodelbello@Amos-MacBook-Pro.local>
2023-05-20 12:57:58 -0600

    Add functionality

--- scratch.el
+++ scratch.el
@@ -1,6 +1,9 @@
 (defvar dotenvel-env-filepath (format "%s%s" user-emacs-directory ".env")
   "The path to the .env file.")
 
+(defvar dotenvel-env '()
+  "The alist that stores .env variables.")
+
 (defun get-file-contents (filename)
   "Return the contents of FILENAME."
   (with-temp-buffer
@@ -8,11 +11,43 @@
     (buffer-string)))
 
 (defun file-contents-to-list ()
-  "Read the contents of env file and split by newline."
+  "Read the contents of env file and split elements into a list."
   (split-string (get-file-contents dotenvel-env-filepath) "\\(\n+\\)"))
 
-(setq env (file-contents-to-list))
-(cdr env)
+(defun filter-entries (lst)
+  "Filter out commented lines in LST."
+  (seq-filter
+   (lambda (item) (not (s-starts-with-p "#" item)))
+   lst))
+
+(defun parse-entries (lst)
+  "Split the strings in LST by equal signs and trim whitespace."
+  (mapcar (lambda (entry)
+            (mapcar #'string-trim
+                    (split-string entry "=")))
+          lst))
+
+
+(filter-entries (file-contents-to-list))
+(parse-entries (filter-entries (file-contents-to-list)))
+
+
+(defun dotenvel-load ()
+  "Load the values from .env file."
+  (interactive)
+  (setq dotenvel-env (parse-entries
+                      (filter-entries
+                       (file-contents-to-list)))))
+
+(defun dotenvel-get (field)
+  "Get the value of FIELD from dotenvel-env."
+  (interactive)
+  (car (cdr (assoc field dotenvel-env))))
+
+(dotenvel-get "GOODVAL")
 
 ;; Need functions
 ;; - is valid key/value
+;;   - doesn't start with a #
+;;   - has a single equal sign
+;;   - trim whitespace
