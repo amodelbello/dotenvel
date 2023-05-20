5f2594caf9858ac6d22e34626fd3632a0d7b4941
Amo DelBello <amodelbello@Amos-MacBook-Pro.local>
2023-05-19 22:04:20 -0600

    Add a couple initial functions

--- scratch.el
+++ scratch.el
@@ -1,5 +1,5 @@
 (defvar dotenvel-env-filepath (format "%s%s" user-emacs-directory ".env")
-  "The path to the .env file")
+  "The path to the .env file.")
 
 (defun get-file-contents (filename)
   "Return the contents of FILENAME."
@@ -7,4 +7,12 @@
     (insert-file-contents filename)
     (buffer-string)))
 
-(setq contents (get-file-contents dotenvel-env-filepath))
+(defun file-contents-to-list ()
+  "Read the contents of env file and split by newline."
+  (split-string (get-file-contents dotenvel-env-filepath) "\\(\n+\\)"))
+
+(setq env (file-contents-to-list))
+(cdr env)
+
+;; Need functions
+;; - is valid key/value
