47a51c5187c88cafa1dcba55ede7df94a8a2f6dc
Amo DelBello <amodelbello@Amos-MacBook-Pro.local>
2023-05-20 15:32:38 -0600

    Add documentation

--- dotenvel.el
+++ dotenvel.el
@@ -1,3 +1,46 @@
+;;; dotenvel.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Amo DelBello
+;; Maintainer: Amo DelBello
+;; Version: 1.0.0
+;; Package-Requires: ()
+;; Homepage: https://github.com/amodelbello/dotenvel
+;; Keywords: dotenv, environment, configuration
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; dotenvel is a package that allows the use of environment variables
+;; stored in a .env file, similar to a technique used in the node.js ecosystem.
+;;
+;; VARIABLES:
+;;
+;; dotenvel-env-filepath  : Path to .env file (defaults to .emacs.d/.env)
+;; dotenvel-env           : alist that stores environment variables
+;;
+;; COMMANDS:
+;; dotenvel-load             : Loads the .env file into dotenvel-env
+;;
+;; FUNCTIONS:
+;; dotenvel-get              : Takes a string parameter and returns corresponding value
+
+;;; Code:
+
 (defvar dotenvel-env-filepath (format "%s%s" user-emacs-directory ".env")
   "The path to the .env file.")
 
@@ -30,6 +73,13 @@
                       (split-string quoteless "="))))
           lst))
 
+(defun dotenvel-load ()
+  "Load the values from .env file."
+  (interactive)
+  (setq dotenvel-env (dotenvel-parse-entries
+                      (dotenvel-filter-entries
+                       (dotenvel-file-contents-to-list)))))
+
 (defun dotenvel-get (field &optional default)
   "Get the value of FIELD from dotenvel-env.
 Use DEFAULT if no value is found."
@@ -37,9 +87,6 @@ Use DEFAULT if no value is found."
    (car (cdr (assoc field dotenvel-env)))
    default))
 
-(defun dotenvel-load ()
-  "Load the values from .env file."
-  (interactive)
-  (setq dotenvel-env (dotenvel-parse-entries
-                      (dotenvel-filter-entries
-                       (dotenvel-file-contents-to-list)))))
+(provide 'dotenvel)
+
+;;; dotenvel.el ends here
